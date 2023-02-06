0b7d2db23745bcc7d3cd8c8ca6d57a371122eaa2
FH-77 <120098823+FH-77@users.noreply.github.com>
2023-02-06 07:57:04 +0000

    app-totyuu

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,7 +1,25 @@
 class Admin::GenresController < ApplicationController
   def index
+    @genres = Genre.new
+    @genre = Genre.all
   end
 
   def edit
   end
+
+  def create
+    @genres = Genre.create(genres_params)
+    @genres.save
+    redirect_to admin_genres_path
+  end
+
+  def update
+  end
+
+  private
+
+  def genres_params
+    params.require(:genre).permit(:name)
+  end
+
 end
