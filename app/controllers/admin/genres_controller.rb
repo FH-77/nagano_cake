fdfb3089221ffe815e3fdf60763535358ebedcfd
FH-77 <120098823+FH-77@users.noreply.github.com>
2023-02-08 12:17:09 +0000

    app-totyuu

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -5,20 +5,24 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genres = Genre.find(params[:id])
   end
 
   def create
-    @genres = Genre.create(genres_params)
+    @genres = Genre.create(genre_params)
     @genres.save
     redirect_to admin_genres_path
   end
 
   def update
+    @genres = Genre.find(params[:id])
+    @genres.update(genre_params)
+    redirect_to admin_genres_path
   end
 
   private
 
-  def genres_params
+  def genre_params
     params.require(:genre).permit(:name)
   end
 
