0e1d5db34cb8b61c6892d631b77e73fa0b7fdaa5
FH-77 <120098823+FH-77@users.noreply.github.com>
2023-02-12 13:39:34 +0000

    app-totyuu

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,5 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :orders,only:[:show,:update]
-    resources :order_details,only:[:update]
-    resources :customers,only:[:index,:show,:edit,:update]
-    resources :genres,only:[:index,:edit,:create,:update]
-    resources :items,only:[:index,:new,:show,:edit,:create,:update]
-    root "homes#top"
-  end
- 
-  scope module: :public do
-    resources :addresses,only:[:index,:edit,:create,:update,:destroy]
-    resources :orders,only:[:new,:index,:show,:create]
-    resources :cart_items,only:[:index,:update,:destroy,:create]
-    resources :customers,only:[:show,:edit,:update]
-    resources :items,only:[:index,:show]
-    get "/" => "homes#top"
-    get "/about" => "homes#about",as: "about"
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'customers/confirmation'
-    patch 'customers/withdraw'
-    delete 'cart_items/destroy_all'
-  end
- 
- 
-
-  devise_for :customers,skip:[:passwords], controllers: {
+    devise_for :customers,skip:[:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
@@ -34,5 +8,31 @@ Rails.application.routes.draw do
   sessions: "admin/sessions"
 }
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :orders,only:[:show,:update]
+    resources :order_details,only:[:update]
+    resources :customers,only:[:index,:show,:edit,:update]
+    resources :genres,only:[:index,:edit,:create,:update]
+    resources :items,only:[:index,:new,:show,:edit,:create,:update]
+    root "homes#top"
+  end
+
+  scope module: :public do
+    resources :addresses,only:[:index,:edit,:create,:update,:destroy]
+    resources :orders,only:[:new,:index,:show,:create]
+    resources :cart_items,only:[:index,:update,:destroy,:create]
+    resources :items,only:[:index,:show]
+    get "/customers" => "customers#show"
+    get "/customers/information/edit" => "customers#edit"
+    patch "/customers/information" => "customers#update"
+    get "/" => "homes#top"
+    get "/about" => "homes#about",as: "about"
+    get 'orders/confirm'
+    get 'orders/complete'
+    get 'customers/confirmation'
+    patch 'customers/withdraw'
+    delete 'cart_items/destroy_all'
+  end
+
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
