98854d2c78b809d9fb5fe89e8fb55b02d8ab3171
FH-77 <120098823+FH-77@users.noreply.github.com>
2023-02-15 10:00:51 +0000

    app-totyuu

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,21 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @alladdress = Address.all
   end
 
   def edit
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:customer_id,:postal_code,:address,:name)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,9 @@
 class Public::ItemsController < ApplicationController
   def index
+    @item = Item.all.page(params[:page]).per(8)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
